6ab09577932c504c7d8bd4e204e88a5609e69c64
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-02 18:14:45 -0600

    commit

--- Unit2_store.php
+++ Unit2_store.php
@@ -36,7 +36,7 @@
                         <?php $Product = getProducts(getConnection()); ?>
                         <?php if ($Product): ?>
                                 <?php foreach($Product as $row): ?>
-                                        <option value=<?=$row['id']?> data-image=<?$row['image_name']?> data-stock=<?=$row['in_stock']?> > <?=$row['product_name']?> - $<?=$row['price']?> </option>
+                                        <option data-stock=<?=$row['in_stock']?> value=<?=$row['id']?> data-image=<?$row['image_name']?>  > <?=$row['product_name']?> - $<?=$row['price']?> </option>
                                 <?php endforeach?>
                         <?php endif?>
                         <!-- <option id="gummy_bears" value="Gummy Bears-5" onclick=showImage(value)>Gummy Bears - $5</option>
@@ -72,8 +72,7 @@
 <script>
      $('select').on('change', function (e) {
         var optionSelected = $("option:selected", this);
-        var valueSelected = optionSelected.data("image");
-    $('#picture').attr("src", "images/"+valueSelected);
+    $('#picture').attr("src", "images/"+optionSelected.data("image"));
     stock = optionSelected.data("stock");
     if (stock == 0){
         $('#stock_text').text("OUT OF STOCK");
