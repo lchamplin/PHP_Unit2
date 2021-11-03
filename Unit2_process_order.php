300f70f4ac341a2d13bcf694e1577458a5b66809
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-02 20:22:38 -0600

    process order

--- Unit2_process_order.php
+++ Unit2_process_order.php
@@ -1,20 +1,23 @@
  <?php include 'Unit2_header.php';?>
+<?php include 'Unit2_database.php';?>
 
 
 <?php
-$product = $_POST["product"];
-$quantity = floatval($_POST["quantity"]);
-$price = floatval(explode("-", $_POST["product"])[1]);
-$subtotal = $price * $quantity;
-$tax_price = $subtotal * 1.03;
-$round_price = ceil($tax_price);
+// $product = $_POST["product"];
+// $quantity = floatval($_POST["quantity"]);
+// $price = floatval(explode("-", $_POST["product"])[1]);
+// $subtotal = $price * $quantity;
+// $tax_price = $subtotal * 1.03;
+// $round_price = ceil($tax_price);
 
-$donation = "";
-if($_POST["donate"]){
-        $donation = "Total with donation: $" . strval($round_price);
-}
+// $donation = "";
+// if($_POST["donate"]){
+//         $donation = "Total with donation: $" . strval($round_price);
+// }
 
+$newCust = findCustomer($conn, $_POST['email']);
 ?>
+
 
 
 <html>
@@ -29,11 +32,28 @@ if($_POST["donate"]){
 <body>
 <br>
 <br>
-<p>Thank you for your order, <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?> (<?php echo $_POST["email"]; ?>). </p>
+
+<p>Hello <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?>
+<?php
+if ($newCust) { 
+	echo " - <em>Thank you for becoming a customer!</em></p>"; 
+} else { 
+	echo " - <em>Welcome back!</em></p>"; 
+}?>
+<p>We hope you enjoy your PRODUCT candy!</p>
+<p>Order Details:
+	QUANTITY @ $PRICE: 
+	Tax (3%): 
+	Subtotal:
+	Total:/Total with donation(thank you!):
+</p>
+<p>We will send special offers to EMAIL<p>
+
+<!-- <p>Thank you for your order, <?php echo $_POST["fname"]; ?> <?php echo $_POST["lname"]; ?> (<?php echo $_POST["email"]; ?>). </p>
 <p>You have selected <?php echo $_POST["quantity"]; ?>  <?php echo $product;?> @ $<?php echo $price;?></p>
 <p>Subtotal: $<?php echo $subtotal;?></p>
 <p>Total including tax (3%): $<?php echo $tax_price;?></p>
-<p><?php echo $donation ?></p>
+<p><?php echo $donation ?></p> -->
 
 </body>
 </html>
