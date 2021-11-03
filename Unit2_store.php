07195554570994a94c54895e3b7a93c0c149ca88
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-02 18:52:05 -0600

    commit

--- Unit2_store.php
+++ Unit2_store.php
@@ -36,7 +36,7 @@
                         <?php $Product = getProducts(getConnection()); ?>
                         <?php if ($Product): ?>
                                 <?php foreach($Product as $row): ?>
-                                        <option value=<?=$row['id']?> data-image_stock=<?=$row['image_name']?>)+<?=$row['in_stock']?>  > <?=$row['product_name']?> - $<?=$row['price']?> </option>
+                                        <option value=<?=$row['id']?> data-image_stock=<?=$row['image_name']?>+<?=$row['in_stock']?>  > <?=$row['product_name']?> - $<?=$row['price']?> </option>
                                 <?php endforeach?>
                         <?php endif?>
                         <!-- <option id="gummy_bears" value="Gummy Bears-5" onclick=showImage(value)>Gummy Bears - $5</option>
